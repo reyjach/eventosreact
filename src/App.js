738d407e78551c748476c1706d69ecefe9e42e33
reyjach <reymuerto14@hotmail.com>
2020-01-31 02:23:32 -0500

    se creo el header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React ,{Component} from 'react';
+import Header from './Componentes/Header'
 
 class App extends Component{
 
   render() {
     return (
-
+      <div className="App">
+        <Header></Header>
+      </div>
     );
   }
   
