a71cea3a9ed35c5e91755557e66a0c1ef18a00fb
reyjach <reymuerto14@hotmail.com>
2020-02-01 03:07:08 -0500

    se agrego el componentes de Formulario con su diseño. ya se pueden ver las caterorias

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import React ,{Component} from 'react';
-import Header from './Componentes/Header'
+import Header from './Componentes/Header';
+import Formulario from './Componentes/Formulario';
 
 class App extends Component{
+
+  token = 'XLKNMNM3GLI2HVHF7OTN';
+
+  state = {
+    categorias:[]
+  }
+
+  componentDidMount(){
+    this.obetenerCategorias();
+  }
+
+  obetenerCategorias = async () =>{
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+
+    await fetch(url)
+      .then(respuesta =>{
+        return respuesta.json();
+      })
+      .then(categorias =>{
+        this.setState({
+          categorias:categorias.categories
+        })
+      })
+  }
 
   render() {
     return (
       <div className="App">
         <Header></Header>
+
+        <div className="uk-container">
+          <Formulario categorias={this.state.categorias}></Formulario>
+        </div>
       </div>
     );
   }
+
+  }
+
   
-}
+  
+
 
 export default App;
---
+++ src/Componentes/Formulario.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class Formulario extends Component {
+
+    mostrarOpciones = (key) =>{
+        const categoria = this.props.categorias[key];
+
+        const {id, name_localized} = categoria;
+
+        if(!id || !name_localized) return null;
+
+        return(
+            <option key={id} value="id">{name_localized}</option>
+        )
+    }
+    
+    render() { 
+
+
+        const categorias = Object.keys(this.props.categorias);
+        
+
+        return ( 
+            <form>
+                <fieldset className="uk-fieldset uk-margin">
+                    <legend className="uk-legend uk-text-center">
+                        Busca tu evento por nombre o categorias
+                    </legend>
+
+                    <div className="uk-column-1-3@m uk-margin">
+                        <div className="uk-margin" uk-margin="true">
+                            <input className="uk-input" type="text" placeholder="Nombre de evento o Ciudad"></input>
+                        </div>
+                        <div className="uk-margin" uk-margin="true">
+                            <select className="uk-select">
+                                {categorias.map(this.mostrarOpciones)}
+                            </select>
+                        </div>
+                        <div className="uk-margin" uk-margin="true">
+                            <button className="uk-button uk-button-danger">Buscar</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+         );
+    }
+}
+ 
+export default Formulario;
